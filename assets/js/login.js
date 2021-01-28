890aff139f7ec6e2457b91b8709ccbd607686778
qiuyunlong <282111688@qq.com>
2021-01-28 17:01:38 +0800

    登录与注册开发完毕

---
+++ assets/js/login.js
@@ -0,0 +1,77 @@
+$(function () {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    //从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+
+    //通过form.verify()函数自定义校验规则
+    form.verify({
+        //自定义了pwd的校验规则
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        //确认密码的校验规则
+        // 通过形参拿到的是确认密码框中的内容
+        // 还需要拿到密码框中的内容
+        // 然后进行一次等于的判断
+        // 如果判断失败,则return一个提示消息即可
+        repwd: function (value) {
+            //选择器类名为reg-box元素 包裹的 name属性为password
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd != value) {
+                return '两次输入的密码不一致！'
+            }
+        }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val()
+        }, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg(res.message);
+            $('#link_login').click();
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').on('submit', function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败！')
+                }
+                layer.msg('登陆成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+})
